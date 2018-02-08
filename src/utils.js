3832f369fece2b7214c51501352bf08b8ff5ebe5
Andre Perunicic <andre@intoli.com>
2018-02-08 17:36:02 -0500

    Move extractLessVariables into utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,39 @@
+import fs from 'fs';
+
+import less from 'less';
 import scssToJson from 'scss-to-json';
+
+import ExtractVariablesPlugin from './extractVariablesLessPlugin';
+
+
+/**
+ * Return values of compiled LESS variables from a compilable entry point.
+ * @param  {string} lessEntryPath - Root LESS file from which to extract variables.
+ * @param  {Object} variableOverrides - Variable overrides of the form { '@var': 'value' } to use
+ *   during compilation.
+ * @return {Object} Object of the form { 'variable': 'value' }.
+ */
+export const extractLessVariables = (lessEntryPath, variableOverrides = {}) => {
+  const lessEntry = fs.readFileSync(lessEntryPath, 'utf8');
+  return new Promise(async (resolve, reject) => {
+    try {
+      await less.render(
+        lessEntry,
+        {
+          filename: lessEntryPath,
+          modifyVars: variableOverrides,
+          plugins: [
+            new ExtractVariablesPlugin({
+              callback: variables => resolve(variables),
+            }),
+          ],
+        },
+      );
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 
 
 /**
