d13a3ce9ec0c2df512178a783326bbde441d8b98
Andre Perunicic <andre@intoli.com>
2018-02-08 13:27:14 -0500

    Add loadScssThemeAsLess utility function

---
+++ src/utils.js
@@ -0,0 +1,15 @@
+const scssToJson = require('scss-to-json');
+
+
+/**
+ * Read variables from a scss theme file into a less-formatted dictionary.
+ */
+export const loadScssThemeAsLess = (filePath) => {
+  const rawTheme = scssToJson(filePath);
+  const theme = {};
+  Object.keys(rawTheme).forEach((sassVariableName) => {
+    const lessVariableName = sassVariableName.replace(/^\$/, '@');
+    theme[lessVariableName] = rawTheme[sassVariableName];
+  });
+  return theme;
+};
