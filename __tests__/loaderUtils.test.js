6f217cf8d409072afa6452a5bf8177d84964652f
Andre Perunicic <andre@intoli.com>
2018-03-08 13:33:13 -0500

    Fix miscellaneous linting issues in tests

--- __tests__/loaderUtils.test.js
+++ __tests__/loaderUtils.test.js
@@ -7,21 +7,24 @@ import { getScssThemePath } from '../src/loaderUtils';
 describe('getScssThemePath', () => {
   const referencePath = path.resolve(__dirname, 'data/theme.scss');
   it('extracts the theme path from options first', () => {
+    // eslint-disable-next-line no-unused-vars
     const plugin = new AntdScssThemePlugin('other.scss');
     const scssThemePath = getScssThemePath({ scssThemePath: referencePath });
     expect(scssThemePath).toBe(referencePath);
   });
 
   it('extracts the theme path from plugin when not specified in options', () => {
+    // eslint-disable-next-line no-unused-vars
     const plugin = new AntdScssThemePlugin(referencePath);
     const scssThemePath = getScssThemePath({});
     expect(scssThemePath).toBe(referencePath);
   });
 
   it('throws error when path is not specified', () => {
+    // eslint-disable-next-line no-unused-vars
     const plugin = new AntdScssThemePlugin();
     expect(() => {
-      const scssThemePath = getScssThemePath({});
+      getScssThemePath({});
     }).toThrow(/scss theme file must be specified/);
   });
 });
