6aecd2c476911026e86d0f35ffe616a98149f43b
Andre Perunicic <andre@intoli.com>
2018-03-08 12:52:27 -0500

    Add themify function and SCSS_THEME_PATH option to plugin
    The themify method can be used to extend the options of sass-loader
    and less-loader so that the variables from the scss theme are
    recognized.
    
    The SCSS_THEME_PATH option can be used to specify the theme file
    only once.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
+import { overloadSassLoaderOptions } from './antdSassLoader';
+import { overloadLessLoaderOptions } from './antdLessLoader';
+
+
 class AntdScssThemePlugin {
-  constructor(options) {
-    console.log(options);
+  SCSS_THEME_PATH;
+
+  constructor(scssThemePath) {
+    AntdScssThemePlugin.SCSS_THEME_PATH = scssThemePath;
   }
 
   // eslint-disable-next-line
   apply(compiler) {}
 
-  static enhance(loader, options) {
+  static themify({ loader, options, overloadLoader = true }) {
+    let overloadedLoader;
+    let overloadedOptions;
+    switch (loader) {
+      case 'sass-loader':
+        if (overloadLoader) {
+          overloadedLoader = require.resolve('./antdSassLoader.js');
+        }
+        overloadedOptions = overloadSassLoaderOptions(options);
+        break;
+      case 'less-loader':
+        if (overloadLoader) {
+          overloadedLoader = require.resolve('./antdLessLoader.js');
+        }
+        overloadedOptions = overloadLessLoaderOptions(options);
+        break;
+      default:
+        overloadedLoader = loader;
+        overloadedOptions = options || {};
+        break;
+    }
+
     return {
-      loader,
-      options,
+      loader: overloadedLoader,
+      options: overloadedOptions,
     };
   }
 }
