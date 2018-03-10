75a096144e19cee1c79cf510cc5718ebda698a43
Andre Perunicic <andre@intoli.com>
2018-03-09 20:53:25 -0500

    Update App component to use plugin's features

--- examples/example-webpack-3/src/components/App.jsx
+++ examples/example-webpack-3/src/components/App.jsx
@@ -2,16 +2,21 @@ import { Button } from 'antd';
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
+import styles from './App.scss';
+
 
 const ButtonGroup = Button.Group;
 
 const App = () => (
-  <ButtonGroup style={{ padding: 10 }}>
-    <Button type="primary">Primary</Button>
-    <Button>Default</Button>
-    <Button type="dashed">Dashed</Button>
-    <Button type="danger">Danger</Button>
-  </ButtonGroup>
+  <div className={styles.container}>
+    <h1 className={styles.header}>The Color of This Header Is <code>@blue-10</code></h1>
+    <ButtonGroup>
+      <Button type="primary">Primary</Button>
+      <Button>Default</Button>
+      <Button type="dashed">Dashed</Button>
+      <Button type="danger">Danger</Button>
+    </ButtonGroup>
+  </div>
 );
 
 
