0ad56c748e3bf31214f009de1e468acbf049f5aa
Andre Perunicic <andre@intoli.com>
2018-07-12 19:53:02 -0400

    Only register the theme if it is defined through the constructor

--- src/index.js
+++ src/index.js
@@ -14,16 +14,18 @@ class AntdScssThemePlugin {
     const afterEmit = (compilation, callback) => {
       // Watch the theme file for changes.
       const theme = AntdScssThemePlugin.SCSS_THEME_PATH;
-      if (
-        Array.isArray(compilation.fileDependencies)
-        && !compilation.fileDependencies.includes(theme)
-      ) {
-        compilation.fileDependencies.push(theme);
-      } else if (
-        compilation.fileDependencies instanceof Set
-        && !compilation.fileDependencies.has(theme)
-      ) {
-        compilation.fileDependencies.add(theme);
+      if (theme) {
+        if (
+          Array.isArray(compilation.fileDependencies)
+          && !compilation.fileDependencies.includes(theme)
+        ) {
+          compilation.fileDependencies.push(theme);
+        } else if (
+          compilation.fileDependencies instanceof Set
+          && !compilation.fileDependencies.has(theme)
+        ) {
+          compilation.fileDependencies.add(theme);
+        }
       }
       callback();
     };
